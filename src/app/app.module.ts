b350edd6739cd31ac68fe8e1cb0a8d5bc7726ec2
praveen <paarveen00@gmail.com>
2026-04-17 11:35:09 +0530

    Disable client hydration for static GitHub Pages build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -32,7 +32,6 @@ import { MaterialModule } from './material/material.module';
     MaterialModule,
   ],
   providers: [
-    provideClientHydration(),
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
