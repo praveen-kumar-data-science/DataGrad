ae9c1220dd4ee6ce119c47092a9e31f000e1bb66
praveen <paarveen00@gmail.com>
2026-04-17 11:59:27 +0530

    Simplify routes to landing page for stable Pages runtime

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
 import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LeftCarouselComponent } from './components/carousel/left-carousel/left-carousel.component';
-import { RightCarouselComponent } from './components/carousel/right-carousel/right-carousel.component';
 import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
 const routes: Routes = [
   {
     path:'',component:HeroBannerComponent
-  },
-  {
-    path:'right',component:RightCarouselComponent
-  },
-  {
-    path:'left',component:LeftCarouselComponent
   }
 ];
 
